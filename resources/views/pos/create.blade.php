430acceafa1defa607c263360aaa3bee35c4bc9f
putrawijayaa <utaw21@gmail.com>
2025-04-28 14:01:26 +0700

    update

--- resources/views/pos/create.blade.php
+++ resources/views/pos/create.blade.php
@@ -8,7 +8,7 @@
     <title>Point Of Sales</title>
     <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('assets/kasir/css/style.css') }}">
-    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('assets/img/404.png') }}" type="image/x-icon">
     <script src="https://unpkg.com/idb/build/iife/index-min.js"></script>
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.2/dist/alpine.min.js"></script>
 
@@ -19,152 +19,152 @@
     </style>
 
     <script>
-    function initApp() {
-        const app = {
-            db: null,
-            time: null,
-            firstTime: localStorage.getItem("first_time") === null,
-            activeMenu: 'pos',
-            loadingSampleData: false,
-            moneys: [2000, 5000, 10000, 20000, 50000, 100000],
-            products: @json($Products),
-            keyword: "",
-            cart: [],
-            cash: 0,
-            change: 0,
-            isShowModalReceipt: false,
-            receiptNo: null,
-            receiptDate: null,
-            filteredProducts() {
-                const rg = this.keyword ? new RegExp(this.keyword, "gi") : null;
-                return this.products.filter((p) => !rg || p.name.match(rg));
-            },
-            addToCart(product) {
-                const index = this.findCartIndex(product);
-                if (index === -1) {
-                    this.cart.push({
-                        productId: product.id,
-                        image: product.image,
-                        name: product.name,
-                        price: product.price,
-                        option: product.option,
-                        qty: 1,
-                    });
-                } else {
-                    this.cart[index].qty += 1;
-                }
-                this.beep();
-                this.updateChange();
-            },
-            findCartIndex(product) {
-                return this.cart.findIndex((p) => p.productId === product.id);
-            },
-            addQty(item, qty) {
-                const index = this.cart.findIndex((i) => i.productId === item.productId);
-                if (index === -1) {
-                    return;
-                }
-                const afterAdd = item.qty + qty;
-                if (afterAdd === 0) {
-                    this.cart.splice(index, 1);
-                    this.clearSound();
-                } else {
-                    this.cart[index].qty = afterAdd;
+        function initApp() {
+            const app = {
+                db: null,
+                time: null,
+                firstTime: localStorage.getItem("first_time") === null,
+                activeMenu: 'pos',
+                loadingSampleData: false,
+                moneys: [2000, 5000, 10000, 20000, 50000, 100000],
+                products: @json($Products),
+                keyword: "",
+                cart: [],
+                cash: 0,
+                change: 0,
+                isShowModalReceipt: false,
+                receiptNo: null,
+                receiptDate: null,
+                filteredProducts() {
+                    const rg = this.keyword ? new RegExp(this.keyword, "gi") : null;
+                    return this.products.filter((p) => !rg || p.name.match(rg));
+                },
+                addToCart(product) {
+                    const index = this.findCartIndex(product);
+                    if (index === -1) {
+                        this.cart.push({
+                            productId: product.id,
+                            image: product.image,
+                            name: product.name,
+                            price: product.price,
+                            option: product.option,
+                            qty: 1,
+                        });
+                    } else {
+                        this.cart[index].qty += 1;
+                    }
                     this.beep();
+                    this.updateChange();
+                },
+                findCartIndex(product) {
+                    return this.cart.findIndex((p) => p.productId === product.id);
+                },
+                addQty(item, qty) {
+                    const index = this.cart.findIndex((i) => i.productId === item.productId);
+                    if (index === -1) {
+                        return;
+                    }
+                    const afterAdd = item.qty + qty;
+                    if (afterAdd === 0) {
+                        this.cart.splice(index, 1);
+                        this.clearSound();
+                    } else {
+                        this.cart[index].qty = afterAdd;
+                        this.beep();
+                    }
+                    this.updateChange();
+                },
+                addCash(amount) {
+                    this.cash = (this.cash || 0) + amount;
+                    this.updateChange();
+                    this.beep();
+                },
+                getItemsCount() {
+                    return this.cart.reduce((count, item) => count + item.qty, 0);
+                },
+                updateChange() {
+                    this.change = this.cash - this.getTotalPrice();
+                },
+                updateCash(value) {
+                    this.cash = parseFloat(value.replace(/[^0-9]+/g, ""));
+                    this.updateChange();
+                },
+                getTotalPrice() {
+                    return this.cart.reduce(
+                        (total, item) => total + item.qty * item.price,
+                        0
+                    );
+                },
+                submitable() {
+                    return this.change >= 0 && this.cart.length > 0;
+                },
+                submit() {
+                    const time = new Date();
+                    this.isShowModalReceipt = true;
+                    this.receiptNo = `TWPOS-KS-${Math.round(time.getTime() / 1000)}`;
+                    this.receiptDate = this.dateFormat(time);
+                },
+                closeModalReceipt() {
+                    this.isShowModalReceipt = false;
+                },
+                dateFormat(date) {
+                    const formatter = new Intl.DateTimeFormat('id', {
+                        dateStyle: 'short',
+                        timeStyle: 'short'
+                    });
+                    return formatter.format(date);
+                },
+                numberFormat(number) {
+                    return (number || "")
+                        .toString()
+                        .replace(/^0|\./g, "")
+                        .replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
+                },
+                priceFormat(number) {
+                    return number ? `Rp. ${this.numberFormat(number)}` : `Rp. 0`;
+                },
+                clear() {
+                    this.cash = 0;
+                    this.cart = [];
+                    this.receiptNo = null;
+                    this.receiptDate = null;
+                    this.updateChange();
+                    this.clearSound();
+                },
+                beep() {
+                    this.playSound("{{ asset('assets/kasir/sound/beep-29.mp3') }}");
+                },
+                clearSound() {
+                    this.playSound("{{ asset('assets/kasir/sound/button-21.mp3') }}");
+                },
+                playSound(src) {
+                    const sound = new Audio();
+                    sound.src = src;
+                    sound.play();
+                    sound.onended = () => delete(sound);
+                },
+                printAndProceed() {
+                    const receiptContent = document.getElementById('receipt-content');
+                    const titleBefore = document.title;
+                    const printArea = document.getElementById('print-area');
+
+                    printArea.innerHTML = receiptContent.innerHTML;
+                    document.title = this.receiptNo;
+
+                    window.print();
+                    this.isShowModalReceipt = false;
+
+                    printArea.innerHTML = '';
+                    document.title = titleBefore;
+
+                    // TODO save sale data to database
+
+                    this.clear();
                 }
-                this.updateChange();
-            },
-            addCash(amount) {
-                this.cash = (this.cash || 0) + amount;
-                this.updateChange();
-                this.beep();
-            },
-            getItemsCount() {
-                return this.cart.reduce((count, item) => count + item.qty, 0);
-            },
-            updateChange() {
-                this.change = this.cash - this.getTotalPrice();
-            },
-            updateCash(value) {
-                this.cash = parseFloat(value.replace(/[^0-9]+/g, ""));
-                this.updateChange();
-            },
-            getTotalPrice() {
-                return this.cart.reduce(
-                    (total, item) => total + item.qty * item.price,
-                    0
-                );
-            },
-            submitable() {
-                return this.change >= 0 && this.cart.length > 0;
-            },
-            submit() {
-                const time = new Date();
-                this.isShowModalReceipt = true;
-                this.receiptNo = `TWPOS-KS-${Math.round(time.getTime() / 1000)}`;
-                this.receiptDate = this.dateFormat(time);
-            },
-            closeModalReceipt() {
-                this.isShowModalReceipt = false;
-            },
-            dateFormat(date) {
-                const formatter = new Intl.DateTimeFormat('id', {
-                    dateStyle: 'short',
-                    timeStyle: 'short'
-                });
-                return formatter.format(date);
-            },
-            numberFormat(number) {
-                return (number || "")
-                    .toString()
-                    .replace(/^0|\./g, "")
-                    .replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
-            },
-            priceFormat(number) {
-                return number ? `Rp. ${this.numberFormat(number)}` : `Rp. 0`;
-            },
-            clear() {
-                this.cash = 0;
-                this.cart = [];
-                this.receiptNo = null;
-                this.receiptDate = null;
-                this.updateChange();
-                this.clearSound();
-            },
-            beep() {
-                this.playSound("sound/beep-29.mp3");
-            },
-            clearSound() {
-                this.playSound("sound/button-21.mp3");
-            },
-            playSound(src) {
-                const sound = new Audio();
-                sound.src = src;
-                sound.play();
-                sound.onended = () => delete(sound);
-            },
-            printAndProceed() {
-                const receiptContent = document.getElementById('receipt-content');
-                const titleBefore = document.title;
-                const printArea = document.getElementById('print-area');
+            };
 
-                printArea.innerHTML = receiptContent.innerHTML;
-                document.title = this.receiptNo;
-
-                window.print();
-                this.isShowModalReceipt = false;
-
-                printArea.innerHTML = '';
-                document.title = titleBefore;
-
-                // TODO save sale data to database
-
-                this.clear();
-            }
-        };
-
-        return app;
-    }
+            return app;
+        }
     </script>
 </head>
 
@@ -236,8 +236,8 @@
                                             <div
                                                 class="transform translate-y-4 opacity-0 group-hover:translate-y-0 group-hover:opacity-100 transition-all duration-300">
                                                 <span class="bg-primary-500 text-white rounded-full p-2 shadow-lg">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none"
-                                                        viewBox="0 0 24 24" stroke="currentColor">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6"
+                                                        fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                         <path stroke-linecap="round" stroke-linejoin="round"
                                                             stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
                                                     </svg>
@@ -266,7 +266,11 @@
                                         <div class="flex items-center mt-2" x-show="product.rating">
                                             <div class="flex">
                                                 <template x-for="i in 5">
-                                                    <svg :class="{'text-yellow-400': i <= Math.round(product.rating), 'text-gray-300': i > Math.round(product.rating)}"
+                                                    <svg :class="{
+                                                        'text-yellow-400': i <= Math.round(product
+                                                            .rating),
+                                                        'text-gray-300': i > Math.round(product.rating)
+                                                    }"
                                                         class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
                                                         <path
                                                             d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
@@ -317,8 +321,8 @@
                                 <!-- trash button -->
                                 <button x-on:click="clear()"
                                     class="text-blue-gray-300 hover:text-pink-500 focus:outline-none">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline-block" fill="none"
-                                        viewBox="0 0 24 24" stroke="currentColor">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline-block"
+                                        fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                             d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                                     </svg>
@@ -330,7 +334,8 @@
                             <template x-for="item in cart" :key="item.productId">
                                 <div
                                     class="select-none mb-3 bg-blue-gray-50 rounded-lg w-full text-blue-gray-700 py-2 px-2 flex justify-center">
-                                    <img :src="item.image" alt="" class="rounded-lg h-10 w-10 bg-white shadow mr-2" />
+                                    <img :src="item.image" alt=""
+                                        class="rounded-lg h-10 w-10 bg-white shadow mr-2" />
                                     <div class="flex-grow">
                                         <h5 class="text-sm" x-text="item.name"></h5>
                                         <p class="text-xs block" x-text="priceFormat(item.price)"></p>
@@ -406,10 +411,12 @@
                                     d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5" />
                             </svg>
                         </div>
-                        <button class="text-white rounded-2xl text-lg w-full py-3 focus:outline-none" x-bind:class="{
+                        <button class="text-white rounded-2xl text-lg w-full py-3 focus:outline-none"
+                            x-bind:class="{
                                 'bg-cyan-500 hover:bg-cyan-600': submitable(),
                                 'bg-blue-gray-900': !submitable()
-                            }" :disabled="!submitable()" x-on:click="submit()">
+                            }"
+                            :disabled="!submitable()" x-on:click="submit()">
                             SUBMIT
                         </button>
                     </div>
@@ -436,7 +443,8 @@
                 x-transition:leave-end="opacity-0 transform scale-90">
                 <div id="receipt-content" class="text-left w-full text-sm p-6 overflow-auto">
                     <div class="text-center">
-                        <img src="{{asset('assets/img/404.png')}}" alt="Tailwind POS" class="mb-3 w-8 h-8 inline-block" />
+                        <img src="{{ asset('assets/img/404.png') }}" alt="Tailwind POS"
+                            class="mb-3 w-8 h-8 inline-block" />
                         <h2 class="text-xl font-semibold">Poin Of Sales</h2>
                         <p>PPKD JAKARTA PUSAT</p>
                     </div>
